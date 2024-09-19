3ebb65547a24e3c1a270e01bc8cdaa58f2601ce3
Anthony Powell <powell.anthonyd@gmail.com>
2024-09-19 08:28:06 -0400

    Configure shadcn ui

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,3 +1,26 @@
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <section className="h-full w-full flex flex-col justify-center items-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Login with Spotify</CardTitle>
+          <CardDescription>
+            Login to your Spotify account to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button>Login with Spotify</Button>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
